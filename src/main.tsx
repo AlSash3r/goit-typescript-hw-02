7336a02ff055c0a0655c0fc577361ea3a36bfa77
Olexandra Ghordievska <maynarisochka@gmail.com>
2025-03-18 13:12:05 +0200

    fix tsx files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import "modern-normalize";
+import "./index.css";
+import App from "./App.jsx";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Toaster
+      toastOptions={{
+        error: {
+          style: {
+            background: "#213547",
+            color: "#f0f0f0",
+          },
+        },
+      }}
+    />
+  </React.StrictMode>
+);
